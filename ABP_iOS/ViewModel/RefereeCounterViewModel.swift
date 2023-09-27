96d2f2c7036f0b4e0163167544e860ae76f674e6
shin-jinwoo <jinu712@gmail.com>
2023-09-27 14:43:55 +0900

    빈 화면 터치시 키패드 사라지게 하는 Extension 추가 및 적용
    심판카운트기 VM 추가

---
+++ ABP_iOS/ViewModel/RefereeCounterViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  RefereeCounterViewModel.swift
+//  ABP_iOS
+//
+//  Created by 신진우 on 9/27/23.
+//
+
+import Foundation
+import Combine
+
+
+
+class RefereeCounterViewModel {
+    
+    
+    init() {
+        
+    }
+    
+    
+}
