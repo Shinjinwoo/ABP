d13bbc6a87992fcd0cb49117cb7a5284d0cd10f1
shin-jinwoo <jinu712@gmail.com>
2023-10-03 17:41:32 +0900

    searchResultView Controller 코드정리

--- ABP_iOS/StadiumWether/View/SearchStadiumLocationViewController.swift
+++ ABP_iOS/StadiumWether/View/SearchStadiumLocationViewController.swift
@@ -15,14 +15,10 @@ class SearchStadiumLocationViewController: UITableViewController {
     
     @IBOutlet var tableView2: UITableView!
     
-    let tableViewCellIdentifier = "SearchStadiumCell"
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        //self.tableView2.register(SearchStadiumCell.self, forCellReuseIdentifier: tableViewCellIdentifier)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,15 +29,10 @@ class SearchStadiumLocationViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //let cell = tableView.dequeueReusableCell(withIdentifier: "SearchStadiumCellTableViewCell", for: indexPath)
-        
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SearchStadiumCellTableViewCell", for: indexPath) as? SearchStadiumCellTableViewCell else {
-//            
-//            print("아아아아악")
-//            return UITableViewCell()
-//        }
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellIdentifier, for: indexPath) as! SearchStadiumCell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SearchStadiumCell", for: indexPath) as? SearchStadiumCell else {
+            
+            return UITableViewCell()
+        }
 
         
         let product = completerResults?[indexPath.row]
