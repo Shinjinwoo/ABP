342cd04460497569cfeffc6b3b03ddf2c699bc40
Shinjinwoo <jinu712@gmail.com>
2023-10-05 17:49:36 +0900

    API 호출시 status 분기처리

--- ABP_iOS/StadiumWether/ViewModel/StadiumWeatherViewModel.swift
+++ ABP_iOS/StadiumWether/ViewModel/StadiumWeatherViewModel.swift
@@ -39,13 +39,19 @@ class StadiumWeatherViewModel {
                         case 200..<300 :
                             print("Success - Status Code: \(statusCode)")
                             self.weatherItems = value.response.body.items.item
-                            
                             //print("Success - Response Value: \(self.weatherItems!)")
-                            
                             print(self.getTimeForWeaterAPI())
                             
+                        case 400..<500:
+                            // 클라이언트 오류 처리
+                            print("Client Error - Status Code: \(statusCode)")
+                            
+                        case 500..<600:
+                            // 서버 오류 처리
+                            print("Server Error - Status Code: \(statusCode)")
                             
                         default:
+                            // 그 외의 상태 코드 처리
                             print("Unknown Status Code: \(statusCode)")
                         }
                     }
@@ -64,11 +70,11 @@ class StadiumWeatherViewModel {
         
         let timeFormatter = DateFormatter()
         timeFormatter.dateFormat = "HH"  // 원하는 날짜 및 시간 형식 지정
-
+        
         
         let currentHour = timeFormatter.string(from: currentTime)+"00"
         let currentDate = dateFormmatter.string(from: currentTime)
-
+        
         
         return (currentDate ,currentHour)
     }
@@ -96,48 +102,6 @@ class StadiumWeatherViewModel {
     }
     
     
-//    {
-//        .responseJSON { response in
-//            switch response.result {
-//            case .success(let value):
-//                if let statusCode = response.response?.statusCode {
-//                    switch statusCode {
-//                    case 200..<300:
-//                        // 성공적인 응답 처리
-//                        print("Success - Status Code: \(statusCode)")
-//                        // JSON 응답 처리
-//                        print("Response JSON: \(value)")
-//                        
-//                        if let jsonData = try? JSONSerialization.data(withJSONObject: value) {
-//                            do {
-//                                let decoder = JSONDecoder()
-//                                let weatherResponse = try decoder.decode(WeatherResponse.self, from: jsonData)
-//                                self.weatherItems = weatherResponse.response.body.items
-//                                // weatherResponse를 사용하여 데이터를 처리합니다.
-//                                print(self.weatherItems!)
-//                            } catch {
-//                                print("JSON 디코딩 오류: \(error)")
-//                            }
-//                        }
-//                    case 400..<500:
-//                        // 클라이언트 오류 처리
-//                        print("Client Error - Status Code: \(statusCode)")
-//                        
-//                    case 500..<600:
-//                        // 서버 오류 처리
-//                        print("Server Error - Status Code: \(statusCode)")
-//                        
-//                    default:
-//                        // 그 외의 상태 코드 처리
-//                        print("Unknown Status Code: \(statusCode)")
-//                    }
-//                }
-//                
-//            case .failure(let error):
-//                print("요청 실패: \(error)")
-//            }
-//        }
-//    }
     func convertToWeatherGrid(latitude: Double, longitude: Double) -> (x: String, y: String) {
         // 기상청 격자 변환 상수
         let RE = 6371.00877
