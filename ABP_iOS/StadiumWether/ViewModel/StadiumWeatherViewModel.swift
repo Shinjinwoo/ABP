32b227d6df80ca159d0a053ba3107a1e3bec6bae
Shinjinwoo <jinu712@gmail.com>
2023-10-05 16:30:37 +0900

    Weather Model 변경

--- ABP_iOS/StadiumWether/ViewModel/StadiumWeatherViewModel.swift
+++ ABP_iOS/StadiumWether/ViewModel/StadiumWeatherViewModel.swift
@@ -10,12 +10,12 @@ import Alamofire
 
 class StadiumWeatherViewModel {
     
-    @Published var weatherItems: WeatherResponse.Response.Body.Items!
+    @Published var weatherItems: [WeatherItem]!
     
     func requestWeatherAPI(latitude:Double,longitude:Double) {
         
         let grid = convertToWeatherGrid(latitude: latitude, longitude: longitude)
-    
+        
         let baseUrl = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
         let parameters =  [
             "serviceKey": Bundle.main.WEATER_API_KEY,
@@ -28,19 +28,22 @@ class StadiumWeatherViewModel {
             "ny": grid.y ]
         
         AF.request(baseUrl, parameters: parameters)
-            .validate()
-            .responseJSON { response in
+            .validate(statusCode: 200..<300)
+            .validate(contentType:["application/json"])
+            .responseDecodable(of:WeatherResponse.self) { response in
                 switch response.result {
-                case .success(let value):
-                    if let jsonData = try? JSONSerialization.data(withJSONObject: value) {
-                        do {
-                            let decoder = JSONDecoder()
-                            let weatherResponse = try decoder.decode(WeatherResponse.self, from: jsonData)
-                            self.weatherItems = weatherResponse.response.body.items
-                            // weatherResponse를 사용하여 데이터를 처리합니다.
-                            print(self.weatherItems!)
-                        } catch {
-                            print("JSON 디코딩 오류: \(error)")
+                case .success(let value) :
+                    if let statusCode = response.response?.statusCode {
+                        switch statusCode {
+                        case 200..<300 :
+                            print("Success - Status Code: \(statusCode)")
+                            self.weatherItems = value.response.body.items.item
+                            
+                            print("Success - Response Value: \(self.weatherItems!)")
+                            self.printGroupdata()
+                            
+                        default:
+                            print("Unknown Status Code: \(statusCode)")
                         }
                     }
                 case .failure(let error):
@@ -49,6 +52,71 @@ class StadiumWeatherViewModel {
             }
     }
     
+    
+    func printGroupdata() {
+        var groupedData: [String: [WeatherItem]] = [:]
+        
+        for item in self.weatherItems {
+            let fcstTime = item.fcstTime
+            if var existingGroup = groupedData[fcstTime] {
+                existingGroup.append(item)
+                groupedData[fcstTime] = existingGroup
+            } else {
+                groupedData[fcstTime] = [item]
+            }
+        }
+        
+        for (fcstTime, items) in groupedData {
+            print("fcstTime: \(fcstTime)")
+            for item in items {
+                print("  \(item)")
+            }
+        }
+    }
+    
+    
+//    {
+//        .responseJSON { response in
+//            switch response.result {
+//            case .success(let value):
+//                if let statusCode = response.response?.statusCode {
+//                    switch statusCode {
+//                    case 200..<300:
+//                        // 성공적인 응답 처리
+//                        print("Success - Status Code: \(statusCode)")
+//                        // JSON 응답 처리
+//                        print("Response JSON: \(value)")
+//                        
+//                        if let jsonData = try? JSONSerialization.data(withJSONObject: value) {
+//                            do {
+//                                let decoder = JSONDecoder()
+//                                let weatherResponse = try decoder.decode(WeatherResponse.self, from: jsonData)
+//                                self.weatherItems = weatherResponse.response.body.items
+//                                // weatherResponse를 사용하여 데이터를 처리합니다.
+//                                print(self.weatherItems!)
+//                            } catch {
+//                                print("JSON 디코딩 오류: \(error)")
+//                            }
+//                        }
+//                    case 400..<500:
+//                        // 클라이언트 오류 처리
+//                        print("Client Error - Status Code: \(statusCode)")
+//                        
+//                    case 500..<600:
+//                        // 서버 오류 처리
+//                        print("Server Error - Status Code: \(statusCode)")
+//                        
+//                    default:
+//                        // 그 외의 상태 코드 처리
+//                        print("Unknown Status Code: \(statusCode)")
+//                    }
+//                }
+//                
+//            case .failure(let error):
+//                print("요청 실패: \(error)")
+//            }
+//        }
+//    }
     func convertToWeatherGrid(latitude: Double, longitude: Double) -> (x: String, y: String) {
         // 기상청 격자 변환 상수
         let RE = 6371.00877
