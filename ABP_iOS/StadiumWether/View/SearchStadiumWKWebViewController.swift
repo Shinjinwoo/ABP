ca617a47ac36ef93dba78e717bac315c9d18ce57
shin-jinwoo <jinu712@gmail.com>
2023-10-08 01:56:28 +0900

    MapKit API 주소검색 능력의 한계가 있음... 다음 주소검색 API WKWebView에 올리기

--- ABP_iOS/StadiumWether/View/SearchStadiumWKWebViewController.swift
+++ ABP_iOS/StadiumWether/View/SearchStadiumWKWebViewController.swift
@@ -6,24 +6,67 @@
 //
 
 import UIKit
+import WebKit
 
 class SearchStadiumWKWebViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let preferences = WKPreferences()
+        /** javaScript 사용 설정 */
+        preferences.javaScriptEnabled = true
+        /** 자동으로 javaScript를 통해 새 창 열기 설정 */
+        preferences.javaScriptCanOpenWindowsAutomatically = true
+        
+        
+        let configuration = WKWebViewConfiguration()
+        /** preference, contentController 설정 */
+        configuration.preferences = preferences
+        
+        
+        let url = "https://searchaddress-eb99b.web.app/"
+        
+        var components = URLComponents(string: url)!
+        let request = URLRequest(url: components.url!)
+        
+        
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        webView.load(request)
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension SearchStadiumWKWebViewController: WKNavigationDelegate {
+    
+    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        print("\(navigationAction.request.url?.absoluteString ?? "")" )
+        
+        decisionHandler(.allow)
+    }
+}
+
+extension SearchStadiumWKWebViewController: WKUIDelegate {
+    
+    public func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        
+    }
+}
+
+extension SearchStadiumWKWebViewController: WKScriptMessageHandler {
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+        print(message.name)
+    }
+}
+
+
