c723bf566b9845dfb995f35f6537d683e99d1e74
Shinjinwoo <jinu712@gmail.com>
2023-10-26 17:48:01 +0900

    Feat : Alamofire API 호출 관련 공통코드
    이번에 학습한 내용 응용하기 : )

---
+++ ABP_iOS/Newtork/APIService.swift
@@ -0,0 +1,93 @@
+//
+//  APIService.swift
+//  AtoStudy_ShinJinwoo
+//
+//  Created by 신진우 on 10/24/23.
+//
+
+import Foundation
+import Combine
+import Alamofire
+
+enum API {
+    case fetchWeatherAPI // 회원가입 요청
+    //case fetchChaters
+    
+    var url : URL {
+        switch self {
+        case .fetchWeatherAPI: return URL(string: "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst")!
+        }
+    }
+}
+
+enum APIService {
+    func fetchWeatherAPI(latitude:Double,longitude:Double) {
+        let grid = convertToWeatherGrid(latitude: latitude, longitude: longitude)
+        self.currentTime = getCurrentTimeForWeaterAPI()
+        
+        let baseUrl = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
+        let parameters =  [
+            "serviceKey": Bundle.main.WEATER_API_KEY,
+            "pageNo": "1",
+            "numOfRows": "120",
+            "dataType": "JSON",
+            "base_date": self.currentTime.currentDate,
+            "base_time": self.currentTime.currentHour,
+            "nx": grid.x,
+            "ny": grid.y ]
+        
+        AF.request(baseUrl, parameters: parameters)
+            .validate(contentType:["application/json"])
+            .responseDecodable(of:WeatherResponse.self) { response in
+                switch response.result {
+                case .success(let value) :
+                    if let statusCode = response.response?.statusCode {
+                        switch statusCode {
+                        case 200..<300 :
+                            print("Success - Status Code: \(statusCode)")
+                            
+                            self.weatherItems = value.response.body.items.item
+                            self.publishedWeatherInfo(targetBaseDate: self.currentTime.currentDate)
+                            
+                        case 400..<500:
+                            // 클라이언트 오류 처리
+                            print("Client Error - Status Code: \(statusCode)")
+                            
+                        case 500..<600:
+                            // 서버 오류 처리
+                            print("Server Error - Status Code: \(statusCode)")
+                            
+                        default:
+                            // 그 외의 상태 코드 처리
+                            print("Unknown Status Code: \(statusCode)")
+                        }
+                    }
+                case .failure(let error):
+                    print("요청 실패: \(error.localizedDescription)")
+                    self.errorFlag = error
+                }
+            }
+    }
+    
+    static func fetchSignUpUserData(snsType:String,nickname:String,character:Int) -> AnyPublisher<SignUpUserResult, AFError> {
+        let headers: HTTPHeaders = [
+            .authorization(bearerToken: Bundle.main.atoApiTokken!)
+        ]
+        
+        let parameters: [String: Any] = [
+            "snsType": snsType,
+            "nickname": nickname,
+            "character": character
+        ]
+        
+        return AF.request(API.fetchUserSignUp.url,
+                          method: .post,
+                          parameters: parameters,
+                          encoding:JSONEncoding.default,
+                          headers: headers)
+            .publishDecodable(type: SignUpUserResult.self)
+            .value()
+            .eraseToAnyPublisher()
+    }
+    
+}
