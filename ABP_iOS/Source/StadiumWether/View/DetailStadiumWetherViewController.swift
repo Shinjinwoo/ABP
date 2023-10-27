09e45f2e73e16ebf123602b4e67d65133f1b7f8d
Shinjinwoo <jinu712@gmail.com>
2023-10-27 13:35:40 +0900

    Feat : 날씨관련 데이터 정리 및 초기구상

--- ABP_iOS/Source/StadiumWether/View/DetailStadiumWetherViewController.swift
+++ ABP_iOS/Source/StadiumWether/View/DetailStadiumWetherViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class DetailStadiumWetherViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstView: UIView!
+    
+    @IBOutlet weak var secondView: UIView!
+    
+    @IBOutlet weak var thirdView: UIView!
+    
+    @IBOutlet weak var skyLabel: UILabel!
+    
     let weatherData:Weather
     
     
@@ -25,5 +33,42 @@ class DetailStadiumWetherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(weatherData)
+        
+        setupUI()
+        
+    }
+    
+    func setupUI() {
+        firstView.layer.cornerRadius = 16
+        secondView.layer.cornerRadius = 16
+        thirdView.layer.cornerRadius = 16
+        
+        
+        // TODO : 하기 데이터 기반으로 화면 구성하기
+        
+        /**
+         
+         SKY : 하늘상태
+         
+         강수확률 {
+            POP : 강수확률
+            PTY : 강수형태
+
+            PCP : 1시간 강수량
+            SNO : 1시간 신적설
+         }
+         
+         기온 {
+            TMP : 1시간 기온,
+            TMN : 일 최저 기온,
+            TMX : 일 최고 기온
+         }
+         
+         홈런 {
+            VEC : 풍향,
+            WSD : 풍속,
+            REH : 습도
+         }
+         */
     }
 }
