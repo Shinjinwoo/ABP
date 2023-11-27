e733a6b06ca700afc57e8423edf8b79770b06b59
Shinjinwoo <jinu712@gmail.com>
2023-11-27 15:21:12 +0900

    Feat: 날씨 API 정보에 따른 날씨 이미지 return 함수 ViewContoller  Extension에 정의

--- ABP_iOS/Resources/Util/UIViewController+Extensions.swift
+++ ABP_iOS/Resources/Util/UIViewController+Extensions.swift
@@ -18,6 +18,37 @@ extension UIViewController {
         view.addGestureRecognizer(tap)
     }
     
+    func configureWeatherImage(SKY: String, PTY: String, fcstTime: String) -> UIImage {
+        if SKY == Sky.Sunny.rawValue && PTY == Pty.Sunny.rawValue{
+            if  fcstTime >= "0600" || fcstTime <= "1800" {
+                return (UIImage(systemName: "sun.max.fill")?.withRenderingMode(.alwaysOriginal))!
+            } else {
+                return (UIImage(systemName: "moon.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(UIColor.yellow))!
+            }
+        }
+        
+        if SKY == Sky.Foggy.rawValue || SKY == Sky.Cloudy.rawValue {
+            switch PTY {
+            case Pty.Sunny.rawValue :
+                return (UIImage(systemName: "cloud.fill")?.withRenderingMode(.alwaysOriginal))!
+                
+            case Pty.Raniy.rawValue :
+                return (UIImage(systemName: "cloud.rain.fill")?.withRenderingMode(.alwaysOriginal))!
+                
+            case Pty.RainyAndSnowy.rawValue :
+                return (UIImage(systemName: "cloud.rain.fill")?.withRenderingMode(.alwaysOriginal))!
+                
+            case Pty.Snowy.rawValue :
+                return (UIImage(systemName: "cloud.snow.fill")?.withRenderingMode(.alwaysOriginal))!
+                
+            default :
+                break
+            }
+        }
+        
+        return UIImage(systemName: "sun.horizon.fill")?.withRenderingMode(.alwaysOriginal) ?? UIImage(systemName: "sun.horizon.fill")!
+    }
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
