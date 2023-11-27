e311214275f05317bdaab9acf62039294f91cb0e
Shinjinwoo <jinu712@gmail.com>
2023-11-27 16:23:03 +0900

    Rename: Lineup -> HomeRunDirection 으로 변경

---
+++ ABP_iOS/Sources/HomeRunDirection/View/HomeRunDirectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeRunDirectionViewController.swift
+//  ABP_iOS
+//
+//  Created by 신진우 on 11/27/23.
+//
+
+import UIKit
+
+class HomeRunDirectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        print("HomeRunDirectionViewController-viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
