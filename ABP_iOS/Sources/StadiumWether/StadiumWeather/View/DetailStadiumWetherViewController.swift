9451b6a65244d294100d0590187d70ee66d2baa6
Shinjinwoo <jinu712@gmail.com>
2023-11-27 15:22:55 +0900

    Design: 날씨 상세뷰 UI 구현

--- ABP_iOS/Sources/StadiumWether/StadiumWeather/View/DetailStadiumWetherViewController.swift
+++ ABP_iOS/Sources/StadiumWether/StadiumWeather/View/DetailStadiumWetherViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class DetailStadiumWetherViewController: UIViewController {
     
-    @IBOutlet weak var timeView: UIView!
     
+    /**
+     헤더 시간 뷰
+     */
+    @IBOutlet weak var headerTimeView: UIView!
     @IBOutlet weak var selectDate: UILabel!
     @IBOutlet weak var selectHour: UILabel!
-    
-    @IBOutlet weak var firstView: UIView!
-    @IBOutlet weak var secondView: UIView!
-    @IBOutlet weak var thirdView: UIView!
+    /**
+     하늘 상태 뷰
+     */
+    @IBOutlet weak var skyInfoView: UIView!
     @IBOutlet weak var skyLabel: UILabel!
+    @IBOutlet weak var skyImageView: UIImageView!
+    /**
+     강수 정보 뷰
+     */
+    @IBOutlet weak var rainInfoView: UIView!
     @IBOutlet weak var oneHourRainFallLabel: UILabel!
-    //@IBOutlet weak var oneHourSnowFall: UILabel!
     @IBOutlet weak var POPLable: UILabel!
     @IBOutlet weak var rainImageView: UIImageView!
-    
-    
+    /**
+     기온 정보 뷰
+     */
+    @IBOutlet weak var tempeInfoView: UIView!
     @IBOutlet weak var temperaturesImage: UIImageView!
     @IBOutlet weak var temperaturesLabel: UILabel!
     
@@ -43,23 +52,26 @@ class DetailStadiumWetherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(weatherData)
+        print(weatherItem)
         setupUI()
     }
     
     func setupUI() {
-        firstView.layer.cornerRadius = 16
-        secondView.layer.cornerRadius = 16
-        thirdView.layer.cornerRadius = 16
-        rainImageView.layer.cornerRadius = 16
-        temperaturesImage.layer.cornerRadius = 16
-        timeView.layer.cornerRadius = 16
+        headerTimeView.layer.cornerRadius = 16
         
+        skyInfoView.layer.cornerRadius = 16
+        skyImageView.layer.cornerRadius = 16
+        
+        rainInfoView.layer.cornerRadius = 16
+        rainImageView.layer.cornerRadius = 16
+        
+        tempeInfoView.layer.cornerRadius = 16
+        temperaturesImage.layer.cornerRadius = 16
             
         let dateFormatterInput = DateFormatter()
         dateFormatterInput.dateFormat = "yyyyMMdd"
 
-        if let date = dateFormatterInput.date(from: weatherData.fcstDate) {
+        if let date = dateFormatterInput.date(from: weatherItem.fcstDate) {
             let dateFormatterOutput = DateFormatter()
             dateFormatterOutput.dateFormat = "yyyy년 MM월 dd일"
             let formattedDate = dateFormatterOutput.string(from: date)
@@ -71,22 +83,35 @@ class DetailStadiumWetherViewController: UIViewController {
         }
         
         
-        selectHour.text = "시각 : \(weatherData.fcstTime)"
+        selectHour.text = "시각 : \(weatherItem.fcstTime)"
         
         var skyState = ""
         
-        switch weatherData.weatherData.SKY {
+        switch weatherItem.weatherData.SKY {
         case Sky.Sunny.rawValue : skyState = Sky.Sunny.state
         case Sky.Foggy.rawValue : skyState = Sky.Foggy.state
         case Sky.Cloudy.rawValue : skyState = Sky.Cloudy.state
         default: break
         }
         
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR") // 한국 로케일 사용
+        dateFormatter.dateFormat = "a h시" // 입력된 문자열 형식
+
+        if let date = dateFormatter.date(from: weatherItem.fcstTime) {
+            dateFormatter.dateFormat = "HHmm" // 출력할 문자열 형식
+            let formattedTime = dateFormatter.string(from: date)
+            skyImageView.image = self.configureWeatherImage(SKY:weatherItem.weatherData.SKY , PTY: weatherItem.weatherData.PTY, fcstTime:formattedTime )
+        } else {
+            print("시간을 변환할 수 없습니다.")
+        }
+        
+        
         skyLabel.text = skyState
-        POPLable.text = "\(weatherData.weatherData.POP)% 확률"
+        POPLable.text = "\(weatherItem.weatherData.POP)% 확률"
         
         if weatherItem.weatherData.PCP == "강수없음" {
-            oneHourRainFallLabel.text = "0.0mm"
+            oneHourRainFallLabel.text = "강수량 0.0mm"
         } else {
             oneHourRainFallLabel.text = "강수량 \(weatherItem.weatherData.PCP)mm"
         }
