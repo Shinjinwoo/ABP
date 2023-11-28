a942fe49259727278c20deb7caa0ffa44b21e731
Shinjinwoo <jinu712@gmail.com>
2023-11-28 09:57:27 +0900

    Feat: 타구방향뷰, 나침반 기능 구현
     CL 기반

--- ABP_iOS/Sources/HomeRunDirection/View/HomeRunDirectionViewController.swift
+++ ABP_iOS/Sources/HomeRunDirection/View/HomeRunDirectionViewController.swift
@@ -6,16 +6,64 @@
 //
 
 import UIKit
+import CoreLocation
 
 class HomeRunDirectionViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setupUI()
+        setupCL()
         print("HomeRunDirectionViewController-viewDidLoad")
-        // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupCL()
+    }
+    
+    private func setupUI() {
+        
+        
+    }
+    
+    private func setupCL() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = 1000
+        grantPermission(locationManager)
+    }
+    
+    
+    private func grantPermission(_ locationManager:CLLocationManager) {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            DispatchQueue.global().async {
+                if CLLocationManager.locationServicesEnabled() {
+                    print("위치 서비스 On 상태")
+                    self.locationManager.startUpdatingHeading()
+                }
+            }
+            
+        case .denied, .restricted:
+            print("위치 서비스 Off 상태")
+            // 위치 권한이 거부되었거나 제한되었을 때 처리할 작업을 수행합니다.
+        default:
+            break
+        }
+    }
+}
 
 
+extension HomeRunDirectionViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        print(newHeading.trueHeading)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        grantPermission(manager)
+    }
 }
