fcf80d147f0319c8a7cfc29159cfbb5deb9c4db4
Shinjinwoo <jinu712@gmail.com>
2023-11-29 14:59:40 +0900

    Design: 타구방향뷰, 하단 정보 UI 구현 및 API 통신 Indicator 구현

--- ABP_iOS/Sources/HomeRunDirection/View/HomeRunDirectionViewController.swift
+++ ABP_iOS/Sources/HomeRunDirection/View/HomeRunDirectionViewController.swift
@@ -26,7 +26,23 @@ class HomeRunDirectionViewController: UIViewController {
     @IBOutlet weak var windImageView: UIImageView!
     @IBOutlet weak var northPointedArrow: UIImageView!
     
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    @IBOutlet weak var windVelocityLabel: UILabel!
+    @IBOutlet weak var humdityLabel: UILabel!
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    @IBOutlet weak var indicatorView: UIView!
     var isFirtstCallHead = true
+    
+    lazy var activityIndicator: UIActivityIndicatorView = { // indicator가 사용될 때까지 인스턴스를 생성하지 않도록 lazy로 선언
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.frame = indicatorView.frame
+        activityIndicator.style = UIActivityIndicatorView.Style.large // indicator의 스타일 설정, large와 medium이 있음
+        activityIndicator.startAnimating() // indicator 실행
+        activityIndicator.isHidden = false
+        return activityIndicator
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +62,8 @@ class HomeRunDirectionViewController: UIViewController {
         locationManager.delegate = nil // delegate에 nil 할당
         locationManager.stopUpdatingLocation() // 위치 업데이트 중지
         locationManager.stopUpdatingHeading() // 방향 정보 업데이트 중지
+        activityIndicator.startAnimating()
+        indicatorView.isHidden = false
     }
     
     deinit {
@@ -57,40 +75,28 @@ class HomeRunDirectionViewController: UIViewController {
     private func setupUI() {
         
         self.bottomView.layer.cornerRadius = 16
-        self.bottomView.backgroundColor = .systemGray4
+        self.bottomView.backgroundColor = .systemBackground
         
+        for case let subview in stackView.arrangedSubviews {
+            subview.layer.cornerRadius = 16 // 설정하고 싶은 radius 값
+            subview.layer.masksToBounds = true // 설정한 radius 값으로 뷰를 자르도록 설정
+            subview.backgroundColor = .systemGray4
+        }
+        
+        indicatorView.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: indicatorView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: indicatorView.centerYAnchor)
+        ])
     }
     
     private func bind() {
         viewModel.$items
             .receive(on: RunLoop.main)
             .sink {list in
-                if let degreeStr = list?[0].weatherData.VEC {
-                    if let degreeFloat = Float(degreeStr), let degree = CGFloat(exactly: degreeFloat) {
-                        print(degree) // 변환된 CGFloat 값 출력
-                        
-                        self.bottomView.translatesAutoresizingMaskIntoConstraints = false
-
-                        UIView.animate(withDuration: 0.5) {
-                            let rotationAngle = CGFloat(degree * Double.pi / 180)
-                            self.windImageView.transform = CGAffineTransform(rotationAngle: rotationAngle).translatedBy(x: self.northPointedArrow.bounds.minX,
-                                                                                                                        y: self.northPointedArrow.bounds.minY)
-                            if let tabBar = self.tabBarController?.tabBar {
-                                UIView.animate(withDuration: 0.5) {
-                                    NSLayoutConstraint.activate([
-                                        self.bottomView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-                                        self.bottomView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
-                                        self.bottomView.heightAnchor.constraint(equalToConstant: 200),
-                                        self.bottomView.bottomAnchor.constraint(equalTo: tabBar.topAnchor, constant: -32 )
-                                    ])
-                                    self.view.layoutIfNeeded()
-                                }
-                            }
-                        }
-                    } else {
-                        print("문자열을 CGFloat으로 변환할 수 없습니다.")
-                    }
-                }
+                self.showWeatherInfo(weather: list?[0])
+                self.indicatorView.isHidden = true
             }.store(in: &subscriptions)
     }
     
@@ -119,6 +125,42 @@ class HomeRunDirectionViewController: UIViewController {
             // 위치 권한이 거부되었거나 제한되었을 때 처리할 작업을 수행합니다.
         default:
             break
+        }
+    }
+    
+    
+    private func showWeatherInfo(weather: Weather?) {
+        self.activityIndicator.stopAnimating()
+        if let degreeStr = weather?.weatherData.VEC {
+            if let degreeFloat = Float(degreeStr), let degree = CGFloat(exactly: degreeFloat) {
+                print(degree) // 변환된 CGFloat 값 출력
+                
+                bottomView.translatesAutoresizingMaskIntoConstraints = false
+                
+                UIView.animate(withDuration: 0.5) {
+                    let rotationAngle = CGFloat(degree * Double.pi / 180)
+                    self.windImageView.transform = CGAffineTransform(rotationAngle: rotationAngle).translatedBy(x: self.northPointedArrow.bounds.minX,
+                                                                                                                y: self.northPointedArrow.bounds.minY)
+                    if let tabBar = self.tabBarController?.tabBar {
+                        UIView.animate(withDuration: 0.5) {
+                            NSLayoutConstraint.activate([
+                                self.bottomView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+                                self.bottomView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+                                self.bottomView.topAnchor.constraint(equalTo: self.indicatorView.topAnchor, constant: 16),
+                                self.bottomView.heightAnchor.constraint(equalToConstant: 200)
+                            ])
+                            self.view.layoutIfNeeded()
+                        }
+                    }
+                    
+                    self.windDirectionLabel.text = "\(degreeStr)°"
+                    self.windVelocityLabel.text = "\(weather?.weatherData.WSD ?? "0" )m/s"
+                    self.humdityLabel.text = "\(weather?.weatherData.REH ?? "0")%"
+                    
+                }
+            } else {
+                print("문자열을 CGFloat으로 변환할 수 없습니다.")
+            }
         }
     }
 }
